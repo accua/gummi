ba6b4681062f84b3f6927f68703fee06d71dcc25
Andrew Accuardi <andrewaccuardi@gmail.com>
2017-02-10 14:38:40 -0800

    add passing integration spec testing the update product function.

--- spec/features/detail_page_function_spec.rb
+++ spec/features/detail_page_function_spec.rb
@@ -7,4 +7,11 @@ describe 'the detail page path' do
     click_on 'Twix'
     expect(page).to have_content 'Twix'
   end
+  it "will update the item" do
+    prod = Product.create(name: 'Twix', cost: '2.25', country: 'USA')
+    visit product_path(prod)
+    fill_in 'product[name]', with: 'Snickers'
+    click_on 'Update Product'
+    expect(page).to have_content 'Snickers'
+  end
 end
